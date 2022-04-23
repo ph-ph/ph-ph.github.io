3ddaabfcb2676ad79f198f86fa0c1871cb0566f4
Dzmitry Kishylau <dzmitry_kishylau@Dzmitrys-MacBook-Pro.local>
2022-04-22 22:03:16 -0700

    Cleanup, update footer and add javascripts

--- themes/chalk/assets/javascripts/application.js
+++ themes/chalk/assets/javascripts/application.js
@@ -6,10 +6,13 @@ $(document).ready(function() {
     $('.scrollappear').scrollAppear();
   }
 
+  // TODO: reenable Zooming, once I figure out how dependencies work
   // Zooming
-  new Zooming(
-    {customSize: '100%', scaleBase: 0.9, scaleExtra: 0}
-  ).listen('.zooming');
+  /**
+   * new Zooming(
+   *   {customSize: '100%', scaleBase: 0.9, scaleExtra: 0}
+   * ).listen('.zooming');
+   */
 
   // Share buttons
   $('.article-share a').on('click', function() {
